3da8820349324d180ae765a02e1333e16225f792
Sean Pursani <seanpursani@gmail.com>
2022-03-09 14:58:38 +0100

    Added Add Course functionality

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
 
   useEffect(() => {
     getCourses();
-  }, []);
+  }, [updated]);
 
   return (
   <Router>
--- src/components/AddCourse/AddCourse.jsx
+++ src/components/AddCourse/AddCourse.jsx
@@ -1,17 +1,46 @@
 import React from 'react'
 import { useState } from 'react';
+import "./AddCourse.scss"
 
 const AddCourse = (props) => {
   const {handleRefresh} = props;
-  const [greeting, setGreeting] = useState({
-    createdBy: "",
-    greeting: "",
-    originCountry: ""
+  const [course, setCourse] = useState({
+    name: "",
+    location: "",
+    duration: "",
+    price: null,
+    summary: ""
   })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('http://localhost:8080/course', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(course)
+    })
+    .then((response) => response.json())
+    .then((json => console.log(json)))
+    .catch(err => console.log(err))
+     e.target.reset();
+     handleRefresh()
+  }
 
 
   return (
-    <div>AddCourse</div>
+    <div className="log-form">
+      <h2>Add A New Course</h2>
+      <form className="log-form__input-fields" onSubmit={handleSubmit}>
+        <input type="text" placeholder="course name" onInput={(e) => setCourse({ ...course, name: e.target.value })} />
+        <input type="text" placeholder="course location" onInput={(e) => setCourse({ ...course, location: e.target.value })} />
+        <input type="text" placeholder="course duration" onInput={(e) => setCourse({ ...course, duration: e.target.value })} />
+        <input type="text" placeholder="course price" onInput={(e) => setCourse({ ...course, price: e.target.value })} />
+        <input type="text" placeholder="course summary" onInput={(e) => setCourse({ ...course, summary: e.target.value })} />
+        <button type="submit" className="btn">Submit</button>
+      </form>
+    </div>
   )
 }
 
